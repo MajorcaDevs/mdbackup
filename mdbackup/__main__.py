6ec7cdc2e4270ea290ae3f390e44c01682b17876
melchor629 <melchor9000@gmail.com>
2018-11-20 21:57:39 +0100

    Do compression of folders only if there are providers

--- mdbackup/__main__.py
+++ mdbackup/__main__.py
@@ -46,76 +46,78 @@ def main():
     final_items = []
     items_to_remove = []
 
-    #Compress directories
-    for item in backup.iterdir():
-        #Compress if it is a directory
-        if item.is_dir():
-            filename = item.parts[-1] + '.tar'
-            directory = item.relative_to(backup)
-            logger.info(f'Compressing directory {item} into {filename}')
-            #If a GPG passphrase is defined, compress and cypher using GPG
-            if 'gpg_passphrase' in config['env']:
-                filename += '.gpg'
-                end_cmd = f'| gpg --output "{filename}" --batch --passphrase "{config["env"]["gpg_passphrase"]}" --symmetric -'
+    #(do the following only if there are any providers defined)
+    if len(config['providers']) > 0:
+        #Compress directories
+        for item in backup.iterdir():
+            #Compress if it is a directory
+            if item.is_dir():
+                filename = item.parts[-1] + '.tar'
+                directory = item.relative_to(backup)
+                logger.info(f'Compressing directory {item} into {filename}')
+                #If a GPG passphrase is defined, compress and cypher using GPG
+                if 'gpg_passphrase' in config['env']:
+                    filename += '.gpg'
+                    end_cmd = f'| gpg --output "{filename}" --batch --passphrase "{config["env"]["gpg_passphrase"]}" --symmetric -'
+                else:
+                    filename += '.gz'
+                    end_cmd = f'| gzip > "{filename}"'
+
+                #Do the compression
+                logger.debug(f'Executing command ["bash", "-c", \'tar -c "{str(directory)}" {end_cmd}\']')
+                _exec = subprocess.run(['bash', '-c', f'tar -c "{str(directory)}" {end_cmd}'],
+                                    cwd=str(backup), check=True)
+                final_items.append(Path(backup, filename))
+                items_to_remove.append(Path(backup, filename))
             else:
-                filename += '.gz'
-                end_cmd = f'| gzip > "{filename}"'
+                final_items.append(item)
 
-            #Do the compression
-            logger.debug(f'Executing command ["bash", "-c", \'tar -c "{str(directory)}" {end_cmd}\']')
-            _exec = subprocess.run(['bash', '-c', f'tar -c "{str(directory)}" {end_cmd}'],
-                                   cwd=str(backup), check=True)
-            final_items.append(Path(backup, filename))
-            items_to_remove.append(Path(backup, filename))
-        else:
-            final_items.append(item)
+        try:
+            #Upload files to storage providers
+            backup_folder_name = backup.relative_to(backups_path).parts[0]
+            for prov_config in config['providers']:
+                gd = None
+                #Detect provider type and instantiate it
+                if 'gdrive' == prov_config['type']:
+                    logger.info('Preparing upload to Google Drive')
+                    gd = GDriveStorage(prov_config.get('clientSecrets'), prov_config.get('authTokens'))
 
-    try:
-        #Upload files to storage providers
-        backup_folder_name = backup.relative_to(backups_path).parts[0]
-        for prov_config in config['providers']:
-            gd = None
-            #Detect provider type and instantiate it
-            if 'gdrive' == prov_config['type']:
-                logger.info('Preparing upload to Google Drive')
-                gd = GDriveStorage(prov_config.get('clientSecrets'), prov_config.get('authTokens'))
-
-            if gd is not None:
-                #Create folder for this backup
-                try:
-                    backup_cloud_folder = gd.create_folder(backup_folder_name, prov_config['backupsPath'])
-                except Exception:
-                    #If we cannot create it, will continue to the next configured provider
-                    logger.exception(f'Could not create folder {backup_folder_name}')
-                    continue
-
-                #Upload every file
-                for item in final_items:
+                if gd is not None:
+                    #Create folder for this backup
                     try:
-                        logger.info(f'Uploading {item} to {backup_cloud_folder}')
-                        gd.upload(item, backup_cloud_folder)
+                        backup_cloud_folder = gd.create_folder(backup_folder_name, prov_config['backupsPath'])
                     except Exception:
-                        #Log only in case of error (tries to upload as much as it can)
-                        logger.exception(f'Could not upload file {item}')
-            else:
-                #The provider is invalid, show error
-                logger.error(f'Unknown storage provider "{prov_config["type"]}", ignoring...')
-    finally:
-        #Remove compressed directories
-        for item in items_to_remove:
-            logger.info(f'Removing file from compressed directory {item}')
-            item.unlink()
+                        #If we cannot create it, will continue to the next configured provider
+                        logger.exception(f'Could not create folder {backup_folder_name}')
+                        continue
 
-        #Cleanup old backups
-        max_backups = config.get('maxBackupsKept', 7)
-        backups_list = get_backup_folders_sorted(backups_path)
-        logger.debug('List of folders available:\n{}'.format('\n'.join(backups_list)))
-        for old in backups_list[0:(len(backups_list)-max_backups)]:
-            logger.warn(f'Removing old backup folder {old}')
-            try:
-                shutil.rmtree(old)
-            except Exception:
-                logger.exception(f'Could not remove completely {old}')
+                    #Upload every file
+                    for item in final_items:
+                        try:
+                            logger.info(f'Uploading {item} to {backup_cloud_folder}')
+                            gd.upload(item, backup_cloud_folder)
+                        except Exception:
+                            #Log only in case of error (tries to upload as much as it can)
+                            logger.exception(f'Could not upload file {item}')
+                else:
+                    #The provider is invalid, show error
+                    logger.error(f'Unknown storage provider "{prov_config["type"]}", ignoring...')
+        finally:
+            #Remove compressed directories
+            for item in items_to_remove:
+                logger.info(f'Removing file from compressed directory {item}')
+                item.unlink()
+
+    #Cleanup old backups
+    max_backups = config.get('maxBackupsKept', 7)
+    backups_list = get_backup_folders_sorted(backups_path)
+    logger.debug('List of folders available:\n{}'.format('\n'.join(backups_list)))
+    for old in backups_list[0:(len(backups_list)-max_backups)]:
+        logger.warn(f'Removing old backup folder {old}')
+        try:
+            shutil.rmtree(old)
+        except Exception:
+            logger.exception(f'Could not remove completely {old}')
 
 
 if __name__ == '__main__':
