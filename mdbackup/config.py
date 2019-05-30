fc4e4603600d59bb5fc1c9ccaca790259a51c0e0
melchor629 <melchor9000@gmail.com>
2019-05-30 23:33:20 +0200

    Improve Secrets configuration

--- mdbackup/config.py
+++ mdbackup/config.py
@@ -55,11 +55,11 @@ class ProviderConfig(object):
 
 class SecretConfig(object):
     def __init__(self, secret_type: str, secret_env: Optional[Dict[str, str]], secret_config: Dict[str, any],
-                 secret_providers: Optional[List[str]]):
+                 secret_storage: Optional[List[str]]):
         self.__type = secret_type
         self.__config = secret_config
         self.__env = secret_env if secret_env is not None else {}
-        self.__providers = secret_providers if secret_providers is not None else []
+        self.__storage = secret_storage if secret_storage is not None else []
 
     @property
     def type(self) -> str:
@@ -74,8 +74,8 @@ class SecretConfig(object):
         return self.__env
 
     @property
-    def providers(self) -> List[str]:
-        return self.__providers
+    def storage(self) -> List[Union[str, Dict[str, str]]]:
+        return self.__storage
 
 
 class Config(object):
