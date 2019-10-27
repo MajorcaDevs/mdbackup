eed64a50e9082c5b1b6913c1b6192043b4a4a7df
melchor629 <melchor9000@gmail.com>
2019-10-27 17:44:35 +0100

    docs/ci: base for deployment of docs

--- docs/docker/versioning-build.sh
+++ docs/docker/versioning-build.sh
@@ -12,29 +12,34 @@ function build_docs {
   #deactivate
   #rm -rf .venv
 
-  jq ". |= .+ [\"$1\"]" build/docs/versions.json | sponge build/docs/versions.json
+  jq ". |= .+ [{ \"title\": \"$1\", \"path\": \"$HOMEPAGE_URL/$1\" }]" build/docs/versions.json | sponge build/docs/versions.json
+  echo "<script>window.app.version = '${1}'</script>" >> build/docs/$1/index.html
+  echo "<script>window.app.homepage = '${HOMEPAGE_URL}'</script>" >> build/docs/$1/index.html
+  echo "<script src=\"$HOMEPAGE_URL/version-menu.js\"></script>" >> build/docs/$1/index.html
 }
 
 git checkout -- .
-git clean -fdx .
+# git clean -fdx .
 
 mkdir -p build/docs
 printf '[]' > build/docs/versions.json
+cp docs/docker/version-menu.js build/docs
+echo "<html><head></head><body><script>window.location='$HOMEPAGE_URL/stable/'</script></body></html>" > build/docs/index.html
 
 python -m venv .venv
 . .venv/bin/activate
+
+echo Building docs for master branch
+git checkout master
+build_docs "stable"
 
 echo Building docs for dev branch
 git checkout dev
 build_docs "dev"
 
-echo Building docs for master branch
-git checkout master
-build_docs "latest"
-
 for tag in `git tag | egrep '^v\d+\.\d+\.\d+$' | sort`; do
   git checkout $tag
-  if [[ -d docs ]]; then
+  if [[ -d docs ]] && [[ -f docs/requirements.txt ]]; then
     echo Building docs for version $tag
     build_docs "${tag:1}"
   else
