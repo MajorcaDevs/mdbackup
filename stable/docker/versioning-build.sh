c2f643d6f44dd61639074754638d69d482937258
melchor629 <melchor9000@gmail.com>
2020-03-29 20:59:46 +0000

    Updates

--- stable/docker/versioning-build.sh
+++ stable/docker/versioning-build.sh
@@ -7,16 +7,20 @@ function build_docs {
   #. .venv/bin/activate
 
   pip install -r docs/requirements.txt
-  mkdocs build --config-file=`ls mkdocs.y*ml` --site-dir=build/docs/$1
+  mkdocs build --config-file="$(ls mkdocs.y*ml)" --site-dir="build/docs/$1"
 
   #deactivate
   #rm -rf .venv
 
   jq ". |= .+ [{ \"title\": \"$1\", \"path\": \"$HOMEPAGE_URL/$1\" }]" build/docs/versions.json | sponge build/docs/versions.json
-  echo "<script>window.app.version = '${1}'</script>" >> build/docs/$1/index.html
-  echo "<script>window.app.homepage = '${HOMEPAGE_URL}'</script>" >> build/docs/$1/index.html
-  echo "<script src=\"$HOMEPAGE_URL/version-menu.js\"></script>" >> build/docs/$1/index.html
+  {
+    echo "<script>window.app.version = '${1}'</script>"
+    echo "<script>window.app.homepage = '${HOMEPAGE_URL}'</script>"
+    echo "<script src=\"$HOMEPAGE_URL/version-menu.js\"></script>"
+  } >> "build/docs/$1/index.html"
 }
+
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 git checkout -- .
 git clean -fdx .
@@ -39,16 +43,19 @@ git checkout dev
 git pull
 build_docs "dev"
 
-for tag in `git tag | egrep '^v\d+\.\d+\.\d+$' | sort`; do
-  git checkout $tag
+git checkout "${current_branch}"
+
+for tag in $(git tag | grep -E '^v?\d+\.\d+\.\d+' | python ./docs/docker/versioning-build-grab-latest-minor.py); do
+  echo " > ${tag}"
+  git checkout "$tag"
   if [[ -d docs ]] && [[ -f docs/requirements.txt ]]; then
-    echo Building docs for version $tag
+    echo Building docs for version "$tag"
     build_docs "${tag:1}"
   else
-    echo Version $tag has no docs
+    echo Version "$tag" has no docs
   fi
 done
 
-if [[ ! -z "${BRANCH_NAME}" ]]; then
-  git checkout $BRANCH_NAME
+if [[ -n "${BRANCH_NAME}" ]]; then
+  git checkout "$BRANCH_NAME"
 fi
