04776af517981e45d6595389eae7809f03abc03e
melchor629 <melchor9000@gmail.com>
2020-07-25 17:59:14 +0200

    docs+fix: fix change version menu styling

--- docs/docker/version-menu.js
+++ docs/docker/version-menu.js
@@ -5,10 +5,12 @@
   const addMaterialMenu = (elt, versions) => {
     const current = versions.find(value => value.title === window.app.version)
 
+    // menu item (the whole container)
     const rootLi = document.createElement('li')
     rootLi.classList.add('md-nav__item')
     rootLi.classList.add('md-nav__item--nested')
 
+    // the "button" that triggers the menu change
     const input = document.createElement('input')
     input.classList.add('md-toggle')
     input.classList.add('md-nav__toggle')
@@ -18,6 +20,7 @@
 
     rootLi.appendChild(input)
 
+    // the "label"/text of the menu item
     const lbl01 = document.createElement('label')
     lbl01.classList.add('md-nav__link')
     lbl01.setAttribute('for', 'nav-10000000')
@@ -25,19 +28,37 @@
 
     rootLi.appendChild(lbl01)
 
+    // the icon in the menu item that says "hey there's more if you click here"
+    const icon01 = document.createElement('span')
+    icon01.classList.add('md-nav__icon')
+    icon01.classList.add('md-icon')
+    icon01.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8.59 16.58L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.42z"></path></svg>'
+
+    lbl01.appendChild(icon01)
+
+    // the contents of the new menu
     const nav = document.createElement('nav')
     nav.classList.add('md-nav')
-    nav.setAttribute('data-md-component', 'collapsible')
+    nav.setAttribute('aria-label', current.title)
     nav.setAttribute('data-md-level','1')
 
     rootLi.appendChild(nav)
 
+    // the menu header
     const lbl02 = document.createElement('label')
     lbl02.classList.add('md-nav__title')
     lbl02.setAttribute('for', 'nav-10000000')
     lbl02.textContent = current.title + ' '
 
     nav.appendChild(lbl02)
+
+    // the icon in the menu item header to go back
+    const icon02 = document.createElement('span')
+    icon02.classList.add('md-nav__icon')
+    icon02.classList.add('md-icon')
+    icon02.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 11v2H8l5.5 5.5-1.42 1.42L4.16 12l7.92-7.92L13.5 5.5 8 11h12z"></path></svg>'
+
+    lbl02.appendChild(icon02)
 
     const ul = document.createElement('ul')
     ul.classList.add('md-nav__list')
